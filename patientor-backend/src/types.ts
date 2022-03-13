5bbe066d7ee8bb6f40b8805499c5f1496d961ca5
JJSierra0318 <juanjosesierram@gmail.com>
2022-03-13 10:26:39 -0500

    feat: added Entry tuypes to backend

--- patientor-backend/src/types.ts
+++ patientor-backend/src/types.ts
@@ -10,6 +10,49 @@ export enum Gender {
   Other = 'other'
 }
 
+export enum HealthCheckRating {
+  'Healthy' = 0,
+  'LowRisk' = 1,
+  'HighRisk' = 2,
+  'CriticalRisk' = 3
+}
+
+interface Discharge {
+  date: string,
+  criteria: string
+}
+
+interface SickLeave {
+  startDate: string,
+  endDate: string
+}
+
+interface baseEntry {
+  id: string,
+  description: string,
+  date: string,
+  specialist: string,
+  diagnosisCodes?: Array<DiagnosesEntry['code']>
+}
+
+interface HealthCheckEntry extends baseEntry {
+  type: 'HealthCheck',
+  healthCheckRating: number
+}
+
+interface HospitalEntry extends baseEntry{
+  type: 'Hospital',
+  discharge: Discharge
+}
+
+interface OccupationalHealthcareEntry extends baseEntry{
+  type: 'OccupationalHealthcare',
+  employerName: string,
+  sickLeave?: SickLeave
+}
+
+export type Entry = HospitalEntry | OccupationalHealthcareEntry | HealthCheckEntry;
+
 export interface PatientEntry {
   id: string,
   name: string,
@@ -17,7 +60,7 @@ export interface PatientEntry {
   ssn: string,
   gender: Gender,
   occupation: string,
-  entries: Array<string>
+  entries: Array<Entry>
 }
 
 export type NonSensitivePatientEntry = Omit<PatientEntry, 'ssn' | 'entries'>;
--- patientor-backend/src/utils.ts
+++ patientor-backend/src/utils.ts
@@ -1,4 +1,4 @@
-import { Gender, NewPatientEntry } from "./types";
+import { Gender, NewPatientEntry, Entry } from "./types";
 
 const isString = (text: unknown): text is string => {
   return typeof text === 'string' || text instanceof String;
@@ -49,23 +49,23 @@ const parseGender = (gender: unknown): Gender => {
   return gender;
 };
 
-const parseEntries = (): Array<string> => {
+const parseEntries = (_entries: unknown): Array<Entry> => {
   /*if (!entries || !Array.isArray(entries)) {
     throw new Error('Incorrect or missing entries');
   }*/
-  return [] as Array<string>;
+  return [] as Array<Entry>;
 };
 
-type Fields = { name: unknown, ssn: unknown, dateOfBirth: unknown, occupation: unknown, gender: unknown };
+type Fields = { name: unknown, ssn: unknown, dateOfBirth: unknown, occupation: unknown, gender: unknown, entries: unknown };
 
-const toNewPatientEntry = ({ name, ssn, dateOfBirth, occupation, gender }: Fields): NewPatientEntry => {
+const toNewPatientEntry = ({ name, ssn, dateOfBirth, occupation, gender, entries }: Fields): NewPatientEntry => {
   const newEntry: NewPatientEntry = {
     name: parseName(name),
     ssn: parseSsn(ssn),
     dateOfBirth: parseDate(dateOfBirth),
     occupation: parseOccupation(occupation),
     gender: parseGender(gender),
-    entries: parseEntries()
+    entries: parseEntries(entries)
   };
   return newEntry;
 };
